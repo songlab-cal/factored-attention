7c69e57504d73d186ff22d399a6a16a20abc6f0a
Neil Thomas <thomas.a.neil@gmail.com>
2020-11-10 11:20:39 -0800

    change launch job script to use new wandb project name

--- launch_jobs.sh
+++ launch_jobs.sh
@@ -4,7 +4,7 @@ num_gpus=$(nvidia-smi --query-gpu=name --format=csv,noheader | wc -l)
 let "n = $num_gpus - 1"
 for i in $(seq 0 $n)
 do
-    echo "CUDA_VISIBLE_DEVICES=$i wandb agent proteins/gremlin-contacts/$agent &> $i.out &"
+    echo "CUDA_VISIBLE_DEVICES=$i wandb agent proteins/iclr2021-rebuttal/$agent &> $i.out &"
     # sometimes jobs time out when we run too many, so we increase the timeout
-    CUDA_VISIBLE_DEVICES=$i WANDB_HTTP_TIMEOUT=60 wandb agent proteins/gremlin-contacts/$agent &> $i.out &
+    CUDA_VISIBLE_DEVICES=$i WANDB_HTTP_TIMEOUT=60 wandb agent proteins/iclr2021-rebuttal/$agent &> $i.out &
 done
