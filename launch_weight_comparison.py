f7baf1f966a19e2fec6606db3ce2f0aa1de5a84d
Neil Thomas <thomas.a.neil@gmail.com>
2020-11-17 13:46:30 -0800

    allow for any number of workers and shards

--- launch_weight_comparison.py
+++ launch_weight_comparison.py
@@ -2,11 +2,21 @@ import subprocess
 from multiprocessing import Pool
 
 def run(i):
-    filename = f'fams_0{i}.txt'
+    if i <= 9:
+        str_i = f'0{i}'
+    else:
+        str_i = str(i)
+    filename = f'fams_{str_i}.txt'
     shard = i
     subprocess.run(f'python weight_correlations.py --pdb_filename {filename} --shard {shard}', shell=True)
 
 
 if __name__ == '__main__':
-    with Pool(5) as p:
-        p.map(run, list(range(10)))
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--num_workers", type=int)
+    parser.add_argument("--num_shards", type=int)
+    args = parser.parse_args()
+
+    with Pool(args.num_workers) as p:
+        p.map(run, list(range(args.num_shards)))
