136a78c061ae3eee8b06927f5f82deff865b20fe
Neil Thomas <thomas.a.neil@gmail.com>
2020-11-19 15:08:20 -0800

    remove comment

--- launch_shards.sh
+++ launch_shards.sh
@@ -2,5 +2,5 @@ n=$1
 for i in $(seq 0 $n)
 do
     echo "python weight_correlations.py --shard $i --total_shards $n &"
-    # python weight_correlations.py --shard $i --total_shards $n &
+    python weight_correlations.py --shard $i --total_shards $n &
 done
