0e77a88e1f42a5619aa8ae24ca426e6b6bab10fc
Neil Thomas <thomas.a.neil@gmail.com>
2021-09-22 14:11:11 -0700

    Update data directory

--- download_data.sh
+++ download_data.sh
@@ -12,4 +12,4 @@ wget -P data/5cajA -q -nc https://files.ipd.uw.edu/krypton/5cajA.i90c75.a3m
 wget -P data/5cajA -q -nc https://files.ipd.uw.edu/krypton/5cajA.pdb
 wget -P data/5cajA -q -nc https://files.ipd.uw.edu/krypton/5cajA.cf
 
-# aws s3 cp s3://proteindata/data_raw_pytorch/trrosetta.tar.gz ./data/
+# aws s3 cp s3://songlabdata/proteindata/data_raw_pytorch/trrosetta.tar.gz ./data/
